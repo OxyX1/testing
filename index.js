8f7c969a89c7066d680fdd907f316f80d1f92a35
oxyum services <29GariglioSe@fortcherry.org>
2025-03-27 16:28:32 -0400

    push

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "path")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
