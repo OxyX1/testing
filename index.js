3415c9efe66d3e63d2018417be15a55e7570fbe5
oxyum services <29GariglioSe@fortcherry.org>
2025-03-27 16:30:00 -0400

    push

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
